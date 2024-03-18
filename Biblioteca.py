4b939030b974ba657dd7534976928a60f264e395
albert1413 <albertjovani@iesmontsia.org>
2024-03-18 08:40:34 +0100

    versio 3

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,47 +1,54 @@
+# Definició de la classe Llibre que representa un llibre de la biblioteca
 class Llibre:
     def __init__(self, titol, any, categoria, autor, editorial):
-        self.titol = titol
-        self.any = any
-        self.categoria = categoria
-        self.autor = autor
-        self.editorial = editorial
+        self.titol = titol          # Títol del llibre
+        self.any = any              # Any de publicació del llibre
+        self.categoria = categoria  # Categoria del llibre (objecte de la classe Categoria)
+        self.autor = autor          # Autor del llibre (objecte de la classe Autor)
+        self.editorial = editorial  # Editorial del llibre (objecte de la classe Editorial)
 
+# Definició de la classe Editorial que representa una editorial
 class Editorial:
     def __init__(self, nom, nacionalitat):
-        self.nom = nom
-        self.nacionalitat = nacionalitat
+        self.nom = nom              # Nom de la editorial
+        self.nacionalitat = nacionalitat  # Nacionalitat de la editorial
 
+# Definició de la classe Categoria que representa una categoria de llibre
 class Categoria:
     def __init__(self, tipus, definicio):
-        self.tipus = tipus
-        self.definicio = definicio
+        self.tipus = tipus          # Tipus de categoria
+        self.definicio = definicio  # Definició de la categoria
 
+# Definició de la classe Autor que representa un autor de llibres
 class Autor:
     def __init__(self, nom, data_naixement):
-        self.nom = nom
-        self.data_naixement = data_naixement
+        self.nom = nom              # Nom de l'autor
+        self.data_naixement = data_naixement  # Data de naixement de l'autor
 
+# Definició de la classe Copia que representa una còpia específica d'un llibre
 class Copia:
     def __init__(self, codi_copia, ubicacio):
-        self.codi_copia = codi_copia
-        self.ubicacio = ubicacio
+        self.codi_copia = codi_copia  # Codi identificador de la còpia
+        self.ubicacio = ubicacio      # Ubicació física de la còpia a la biblioteca
 
+# Definició de la classe Prestec que representa un préstec de llibre
 class Prestec:
     def __init__(self, data_inici, data_fi, nom_llibre, nom_lector):
-        self.data_inici = data_inici
-        self.data_fi = data_fi
-        self.nom_llibre = nom_llibre
-        self.nom_lector = nom_lector
+        self.data_inici = data_inici  # Data d'inici del préstec
+        self.data_fi = data_fi        # Data de fi del préstec
+        self.nom_llibre = nom_llibre  # Nom del llibre prestat
+        self.nom_lector = nom_lector  # Nom del lector que ha realitzat el préstec
 
+# Definició de la classe Lector que representa una persona que llegeix llibres
 class Lector:
     def __init__(self, nom, cognoms, dni, adreca):
-        self.nom = nom
-        self.cognoms = cognoms
-        self.dni = dni
-        self.adreca = adreca
+        self.nom = nom              # Nom del lector
+        self.cognoms = cognoms      # Cognoms del lector
+        self.dni = dni              # DNI del lector
+        self.adreca = adreca        # Adreça del lector
 
+# Definició de la classe Multa que representa una multa associada a un préstec
 class Multa:
     def __init__(self, data_inici, data_finalitzacio):
-        self.data_inici = data_inici
-        self.data_finalitzacio = data_finalitzacio
-
+        self.data_inici = data_inici              # Data d'inici de la multa
+        self.data_finalitzacio = data_finalitzacio  # Data de finalització de la multa
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Aquest es el diagrama del codi UML.
 
 ![alt text](image.png)
+
+Tasca 4: Els canvis que s'han fet en el segon codi de classes respecte al codi proporcionat inicialment són:
+
+Al primer, s'han eliminat l'ús de mètodes específics per a l'accés i la modificació dels atributs de les classes. Mentre que en el codi original es defineixen mètodes set i get per a aquesta finalitat.
+
+En el codi original, es feia servir mètodes d'inicialització separats per a cada atribut, mentre que ara tots els valors dels atributs s'assignen directament dins de l'inicialitzador __init__().
+
+En el codi original, els atributs estaven encapsulats dins de mètodes d'accés i modificació, proporcionant un cert grau de protecció i ocultació de les dades. No obstant, en el nou codi els atributs són accessibles directament des de fora de les classes, reduint així el nivell de encapsulació i fent les dades més accessibles.
+
+El nou codi és més concís i senzill, ja que no requereix la definició de mètodes addicionals per a l'accés i la modificació dels atributs, cosa que el fa més fàcil d'entendre i mantenir, especialment en projectes de menor escala o complexitat.
